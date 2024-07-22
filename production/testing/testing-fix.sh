db522ac71ae1ef8766f5341897cdb6c7650bdd6b
m-miftakhul-ulum <201240000992@unisnu.ac.id>
2024-07-22 10:24:28 +0700

    edit bash

--- production/testing/testing-fix.sh
+++ production/testing/testing-fix.sh
@@ -4,15 +4,20 @@ sudo apt update
 
 sudo apt install python3 python3-pip
 
+sudo apt install unzip
+
 pip3 install locust
 
 pip3 install gdown
+
+export PATH="$HOME/.local/bin:$PATH"
+source ~/.bashrc
 
 git clone https://github.com/m-miftakhul-ulum/skripsi.git
 
 sudo apt install python3-locust
 
-pip install --upgrade locust
+pip3 install --upgrade locust
 
 sudo ufw allow 8089/tcp
 
